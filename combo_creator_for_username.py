357dcd175e4d59a8c512da51ced39a42f97b5392
ant0n <69129717+antonchu2006@users.noreply.github.com>
2021-06-17 12:31:18 +0000

    Create combo_creator_for_username.py

---
+++ combo_creator_for_username.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+
+if len(sys.argv) != 3:
+    print("\n[!] Uso: " + sys.argv[0] + " <combo-file> <username to bruteforce>")
+    sys.exit(1)
+
+filename = sys.argv[1]
+output_fname = "combo_" + sys.argv[2] + ".txt"
+output = open(output_fname, 'w')
+
+with open(filename) as f:
+    content = f.readlines()
+
+for line in content:
+    line = line.strip("\n")
+    l = line.split(":")
+    passwd = str(l[1])
+    user = sys.argv[2]
+
+    line_parsed = str(user +  ":" + passwd)
+    output.write(line_parsed + "\n")
+output.close()
